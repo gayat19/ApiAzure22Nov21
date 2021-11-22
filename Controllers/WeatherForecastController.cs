8c9649dc5755d5fd195c70a8905fa25cf6042609
Gayathri Mahadevan <gayat_19@yahoo.com>
2021-11-22 12:21:20 +0530

    CORS again

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -15,7 +15,7 @@ namespace FirstAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Col", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
